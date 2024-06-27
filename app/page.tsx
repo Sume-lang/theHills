19d8416042a32f9caedac9e588c40abab6bbaef3
sumekah33 <sumekah33@gmail.com>
2024-06-27 13:26:32 +0000

    adding and remove unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 import Features from "./sections/Features";
-
-export const revalidate = 0;
-
-export async function generateStaticParams() {}
-
 export default function Home() {
   return (
     <main className="realtive min-h-screen w-full flex flex-col">
--- app/sections/Features.tsx
+++ app/sections/Features.tsx
@@ -29,7 +29,9 @@ export default function Features() {
             </Card>
           ))}
         </div>
+        
       </section>
+
     </main>
   );
 }
