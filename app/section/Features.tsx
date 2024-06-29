bdc35c3e57f599de4c152a016823b26089275f8b
Advsum <sumekah33@gmail.com>
2024-06-29 22:39:39 +0800

    adding the elements

--- app/section/Features.tsx
+++ app/section/Features.tsx
@@ -1,51 +1,13 @@
-"use client";
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import dataServices from "./dataSections";
+import React from "react";
+
 export default function Features() {
-  const [show, setShow] = useState(dataServices[0]);
-  const Toggle = ({ features }: { features: any }) => {
-    setShow(features);
-  };
   return (
-    <AnimatePresence mode="wait">
-      <section className="w-full flex flex-col lg:flex-row items-start justify-start lg:items-start lg:justify-center mt-2 gap-2 p-4">
-        {dataServices.map((item) => (
-          <div
-            key={item.id}
-            className={`grid grid-rows-1 lg:grid-cols-1 w-full md:w-[200px] sm:w-[200px] shadow-md cursor-pointer rounded-md ${
-              item.id === show.id
-                ? "border-slate-900 border"
-                : "border-slate-100"
-            }`}
-            onClick={() => Toggle({ features: item })}
-          >
-            <h1 className="p-2 font-bold text-center uppercase">
-              {item.title}
-            </h1>
-          </div>
-        ))}
+    <main className="flex flex-col w-full max-h-screen bg-slate-400 min-h-screen">
+      <section className="flex flex-row items-center justify-center p-4">
+        <h1 className="lg:text-5xl text-slate-100 font-bold uppercase">
+          What We <span className="text-sky-900">Have?</span>
+        </h1>
       </section>
-      {show && (
-        <div
-          className="w-full min-h-screen flex flex-col items-center justify-start gap-4 mt-10"
-          key={show.id}
-        >
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 0.5,
-              delay: 0.5,
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-            className="w-full min-h-screen flex flex-col items-center justify-start gap-4 mt-10"
-            key={show.id}
-          >
-            {show.packages}
-          </motion.div>
-        </div>
-      )}
-    </AnimatePresence>
+    </main>
   );
 }
