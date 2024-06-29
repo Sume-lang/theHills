b72c2315e6965b9e2f504689005edb4e5523349f
Advsum <sumekah33@gmail.com>
2024-06-29 20:38:52 +0800

    adding small thing

--- app/section/Features.tsx
+++ app/section/Features.tsx
@@ -34,7 +34,13 @@ export default function Features() {
           <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
-            exit={{ x: "-90%", opacity: 0, scale: 0.5 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.5,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            className="w-full min-h-screen flex flex-col items-center justify-start gap-4 mt-10"
+            key={show.id}
           >
             {show.packages}
           </motion.div>
