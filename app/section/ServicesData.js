9f6a4aa7c66c7051e9f315e1961a268cc1fb907f
Advsum <sumekah33@gmail.com>
2024-07-01 15:17:33 +0800

    adding the new files

---
+++ app/section/ServicesData.js
@@ -0,0 +1,20 @@
+const dataServices = [
+  {
+    id: 1,
+    title: "Hiking Rinjani Mountains",
+    img: "",
+    url: "",
+  },
+  {
+    id: 2,
+    title: "Water Activities",
+    img: "",
+    url: "",
+  },
+  {
+    id: 3,
+    title: "Panorama",
+    img: "",
+    url: "",
+  },
+];
